f34f784632c1f92bde6a3509926327b9640269bb
dan <dan.raygo2008@gmail.com>
2026-04-24 11:54:45 +0300

    Initial Commit

---
+++ Driven/DrivenApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrivenApp.swift
+//  Driven
+//
+//  Created by Raygorodsky on 24/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct DrivenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
